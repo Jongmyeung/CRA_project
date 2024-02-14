df564e12b0369ecdb06af2113bc650636af8554d
Jongmyeung Lee <danieljml7love@gmail.com>
2024-02-14 13:50:55 +0900

    feat: connect firestore collection events

--- app/src/main/java/com/example/project/Event1Fragment.kt
+++ app/src/main/java/com/example/project/Event1Fragment.kt
@@ -6,7 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.google.firebase.firestore.FirebaseFirestore
-
+import java.text.SimpleDateFormat
 
 class Event1Fragment : Fragment() {
 
@@ -26,7 +26,10 @@ class Event1Fragment : Fragment() {
             .get()
             .addOnSuccessListener { documents ->
                 for (document in documents) {
-
+                    val timestamp = document.getTimestamp("date")
+                    val date = timestamp?.toDate()
+                    // 날짜와 시간을 포맷하는 형식 지정
+                    val dateFormat = SimpleDateFormat("yyyy")
                 }
             }
     }
