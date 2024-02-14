7205bbde92e0ed475ecb6c405956cd2ae233da24
Jongmyeung Lee <danieljml7love@gmail.com>
2024-02-14 12:15:28 +0900

    feat: fix event data class

--- app/src/main/java/com/example/project/FirebaseDataForEvent.kt
+++ app/src/main/java/com/example/project/FirebaseDataForEvent.kt
@@ -1,10 +1,9 @@
 package com.example.project
 
+import com.google.firebase.Timestamp
+
 data class FirebaseDataForEvent(
     val name : String?,
-    val description : String?,
     val personnel : Int?,
-    val year : Int?,
-    val month : Int?,
-    val day : Int?
+    val time : Timestamp?
 )
