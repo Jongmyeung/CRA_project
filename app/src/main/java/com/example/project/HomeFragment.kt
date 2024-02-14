9a8d910294e3f699f2ca0b4e82f5dc375c8f5793
Jongmyeung Lee <danieljml7love@gmail.com>
2024-02-14 15:59:25 +0900

    feat: complete the move function

--- app/src/main/java/com/example/project/HomeFragment.kt
+++ app/src/main/java/com/example/project/HomeFragment.kt
@@ -30,7 +30,17 @@ class HomeFragment : Fragment() {
         binding.btnToApply.setOnClickListener {
             try {
                 // 신청 버튼을 클릭하면 Event1Fragment로 이동
-                findNavController().navigate(R.id.action_homeFragment_to_event1Fragment)
+                // findNavController().navigate(R.id.action_homeFragment_to_event1Fragment)
+
+                // FragmentManager 사용하기
+                // Fragment Transaction 시작
+                val transaction = requireActivity().supportFragmentManager.beginTransaction()
+                // 이동할 Fragment를 추가하거나 교체
+                transaction.replace(R.id.frame_container, Event1Fragment())
+                // 백스택에 추가하여 이전 Fragment로 돌아갈 수 있도록 함
+                transaction.addToBackStack(null)
+                // Transaction 완료
+                transaction.commit()
             } catch (e: IllegalArgumentException) {
                 // 목적지를 찾을 수 없는 경우 에러 메시지를 로그로 출력
                 Log.e("NavigationError", "Navigation destination not found: ${e.message}")
