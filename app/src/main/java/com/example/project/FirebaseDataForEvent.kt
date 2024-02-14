1c6dda6e32c02e094123693e3bb0950cab745dfb
Jongmyeung Lee <danieljml7love@gmail.com>
2024-02-14 10:48:31 +0900

    feat: add Event1 Activity

--- app/src/main/java/com/example/project/FirebaseDataForEvent.kt
+++ app/src/main/java/com/example/project/FirebaseDataForEvent.kt
@@ -2,6 +2,7 @@ package com.example.project
 
 data class FirebaseDataForEvent(
     val name : String?,
+    val description : String?,
     val numberOfMember : Int?,
     val year : Int?,
     val month : Int?,
--- app/src/main/java/com/example/project/HomeFragment.kt
+++ app/src/main/java/com/example/project/HomeFragment.kt
@@ -1,20 +1,31 @@
 package com.example.project
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.project.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
 
+    private lateinit var binding : FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        binding = FragmentHomeBinding.inflate(inflater)
+
+        binding.btnToApply.setOnClickListener {
+            val intent = Intent(requireContext(), Event1Activity::class.java)
+            startActivity(intent)
+        }
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return binding.root
     }
 }
