12f3be58da3aef87665f74e9793dc1cc714acaf5
Jongmyeung Lee <danieljml7love@gmail.com>
2024-02-20 19:25:47 +0900

    feat: immediate process

--- app/src/main/java/com/example/project/LoadingActivity.kt
+++ app/src/main/java/com/example/project/LoadingActivity.kt
@@ -7,32 +7,50 @@ import android.content.Intent
 import android.os.Handler
 import android.view.View
 import com.example.project.databinding.ActivityLoadingBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class LoadingActivity : AppCompatActivity() {
     private lateinit var binding : ActivityLoadingBinding
+    private lateinit var mAuth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoadingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        mAuth = FirebaseAuth.getInstance()
+    }
 
+    override fun onStart() {
+        super.onStart()
+
+        val currentUser = mAuth!!.currentUser
+        val intent = updateUI(currentUser)
 //        val sharedPref = getSharedPreferences("user_pref", Context.MODE_PRIVATE)
 //        val isMember = sharedPref.getBoolean("is_member", false) // 만약 값이 없으면 false 반환
 
-         val intent = Intent(this, MainActivity::class.java) // 회원가입 한 경우
-         // val intent2 = Intent(this, MainActivity::class.java) // 회원가입 안 한 경우
+//         val intent = Intent(this, MainActivity::class.java) // 회원가입 한 경우
+//         val intent2 = Intent(this, MainActivity::class.java) // 회원가입 안 한 경우
 //        val intent = if (isMember) {
 //            Intent(this, MainActivity::class.java)
 //        } else {
 //            Intent(this, NotMemberMainActivity::class.java)
 //        }
-
         // 경우에 따라 화면 이동이 달라짐
         binding.root.postDelayed({
-            startActivity(intent)
-            finish()
+            if(!isFinishing){
+                startActivity(intent)
+                finish()
+            }
         }, 3000)
+    }
 
+    private fun updateUI(currentUser: FirebaseUser?): Intent {
+        return if (currentUser == null) {
+            Intent(this, NotMemberMainActivity::class.java)
+        } else {
+            Intent(this, MainActivity::class.java)
+        }
     }
 
     override fun onPause() {
