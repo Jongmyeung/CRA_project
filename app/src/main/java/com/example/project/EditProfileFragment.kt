b7f339d27b54ce3fbe8a8f8ea02c5143155a16dc
Jongmyeung Lee <danieljml7love@gmail.com>
2024-02-26 00:35:49 +0900

    feat: immediate process

--- app/src/main/java/com/example/project/EditProfileFragment.kt
+++ app/src/main/java/com/example/project/EditProfileFragment.kt
@@ -5,17 +5,58 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.project.databinding.FragmentEditProfileBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
 
 class EditProfileFragment : Fragment() {
 
+    private lateinit var binding : FragmentEditProfileBinding
+    private val firestore = FirebaseFirestore.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_profile, container, false)
+        binding = FragmentEditProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        firestore.collection("users")
+            .get()
+            .addOnSuccessListener { documents ->
+                val eventStringBuilderName = StringBuilder()
+                val eventStringBuilderEmail = StringBuilder()
+                val eventStringBuilderNickName = StringBuilder()
+                val eventStringBuilderGithub = StringBuilder()
+                val eventStringBuilderStudentNumber = StringBuilder()
+
+                for(document in documents) {
+                    val userName = document.getString("name")
+                    val userNickName = document.getString("nickName")
+                    val userEmail = document.getString("email")
+                    val userGithub = document.getString("Github")
+                    val userStudentNumber = document.getLong("studentNumber")
+
+                    val sentenceName = "이름: ${userName}"
+                    val sentenceEmail = "이메일: ${userEmail}"
+                    val sentenceNickName = "닉네임: ${userNickName}"
+                    val sentenceGithub = "Github: ${userGithub}"
+                    val sentenceStudentNumber = "학번: ${userStudentNumber}"
+
+                    eventStringBuilderName.append(sentenceName).append("\n")
+                    eventStringBuilderEmail.append(sentenceEmail).append("\n")
+                    eventStringBuilderNickName.append(sentenceNickName).append("\n")
+                    eventStringBuilderGithub.append(sentenceGithub).append("\n")
+                    eventStringBuilderStudentNumber.append(sentenceStudentNumber).append("\n")
+                }
+
+            }
+
     }
 
 }
